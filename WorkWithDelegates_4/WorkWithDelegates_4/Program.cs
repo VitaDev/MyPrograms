f058e2c50d9cd6a9eb38d7af3c392bf0cc8b7258
VitaDev <40732826+VitaDev@users.noreply.github.com>
2021-04-24 14:33:35 +0300

    Add files via upload

---
+++ WorkWithDelegates_4/WorkWithDelegates_4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace WorkWithDelegates_4
+{
+    delegate double MyDelegate();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, double> func = (x, y) => 
+            {
+                double result = Math.Pow(x, 2) + Math.Pow(y, 2);
+                Console.WriteLine(result);
+                return result;
+            };
+
+            MyDelegate myDelegate = UsingDelegate(func);
+
+            Console.WriteLine(myDelegate());
+        }
+
+        public static double MathPowSum(int x, int y)
+        {
+            double result = Math.Pow(x, 2) + Math.Pow(y, 2);
+            Console.WriteLine(result);
+            return result;
+        }
+
+        public static MyDelegate UsingDelegate(Func<int, int, double> func)
+        {
+            return () =>
+            {
+                double z = func(10, 20);
+                return Math.Pow(z, 2);
+            };    
+        }
+    }
+}
---
+++ WorkWithThreads1/WorkWithThread/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Threading;
+
+namespace WorkWithThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Главный поток: {Thread.CurrentThread.ManagedThreadId}");
+
+            int size = 20;
+
+            char[] mas = new char[size];
+
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = '*';
+                Console.Write($"| {mas[i]}");
+            }
+            Console.WriteLine("|");
+
+            int firstIndex = 0;
+            int lastIndex = mas.Length-1;
+
+            Thread A, B;
+
+            A = new Thread(() => 
+            {
+                Console.WriteLine($"Вторичный поток: {Thread.CurrentThread.ManagedThreadId}");
+
+                char firstValue = 'Я';
+
+                while (true)
+                {
+                    if (lastIndex > firstIndex)
+                    {
+                        mas[lastIndex] = firstValue;
+                        firstValue--;
+                        lastIndex--;
+
+                        Thread.Sleep(100);
+                    }
+                    else
+                    {
+                        Thread.CurrentThread.Interrupt();
+                        break;
+                    }
+                }
+                Console.WriteLine($"Вторичный поток {Thread.CurrentThread.ManagedThreadId} завершил свою работу");
+            });
+
+            B = new Thread(() =>
+            {
+                Console.WriteLine($"Вторичный поток: {Thread.CurrentThread.ManagedThreadId}");
+
+                char firstValue = 'А';
+
+                while (true)
+                {
+                    if (firstIndex < lastIndex)
+                    {
+                        mas[firstIndex] = firstValue;
+                        firstValue++;
+                        firstIndex++;
+
+                        Thread.Sleep(100);
+                    }
+                    else
+                    {
+                        Thread.CurrentThread.Interrupt();
+                        break;
+                    }
+                }
+                Console.WriteLine($"Вторичный поток {Thread.CurrentThread.ManagedThreadId} завершил свою работу");
+            });
+
+            A.Start();
+            B.Start();
+
+            if (A.IsAlive) A.Join();
+            if (B.IsAlive) B.Join();
+
+            for (int i = 0; i < mas.Length; i++)
+            {
+                Console.Write($"| {mas[i]}");
+            }
+            Console.WriteLine("|");
+
+            Console.WriteLine($"Главный поток {Thread.CurrentThread.ManagedThreadId} завершил свою работу");
+        }
+    }
+}
