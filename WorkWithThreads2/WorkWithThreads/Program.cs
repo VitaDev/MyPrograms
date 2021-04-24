c310b9b4f5ec26a7f60923b87ff286d659b0b279
VitaDev <40732826+VitaDev@users.noreply.github.com>
2021-04-24 14:34:29 +0300

    Add files via upload

---
+++ WorkWithThreads2/WorkWithThreads/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Threading;
+
+namespace WorkWithThreads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Многопоточное вычисление факториала, двойного факториала и числа фибоначи");
+            Console.Write("Для начала вычисления, введите n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Основной поток начал выполнять свою работу:  { Thread.CurrentThread.ManagedThreadId }");
+
+            Thread thread_1 = new Thread(new ParameterizedThreadStart(Calculation.Factorial));
+            thread_1.Start(n);
+
+            Thread thread_2 = new Thread(new ParameterizedThreadStart(Calculation.DoubleFactorial));
+            thread_2.Start(n);
+
+            Thread thread_3 = new Thread(new ParameterizedThreadStart(Calculation.Fibonachi));
+            thread_3.Start(n);
+
+            if (thread_1.IsAlive)
+            {
+                thread_1.Join();
+            }
+
+            if (thread_2.IsAlive)
+            {
+                thread_2.Join();
+            }
+
+            if (thread_3.IsAlive)
+            {
+                thread_3.Join();
+            }
+            Console.WriteLine("Основной поток закончил выполнять свою работу");
+        }
+    }
+
+    class Calculation
+    {
+        public static void Factorial(object num)
+        {
+            Console.WriteLine($"Вторичный поток начал выполнять свою работу:  { Thread.CurrentThread.ManagedThreadId }");
+
+            int n = (int)num;
+
+            double factorial = 1;
+            for (int i = n; i > 1; i--)
+            {
+                factorial *= i;
+                Thread.Sleep(100);
+            }          
+
+            Console.WriteLine($"Вторичный поток закончил выполнять свою работу: { Thread.CurrentThread.ManagedThreadId }");
+
+            Console.WriteLine($"Факториал: {factorial}");
+        }
+
+        public static void DoubleFactorial(object num)
+        {
+            Console.WriteLine($"Вторичный поток начал выполнять свою работу:  { Thread.CurrentThread.ManagedThreadId }");
+
+            int n = (int)num;
+
+            double factorial = 1;
+            for (int i = n; i > 1 || i > 2; i -= 2)
+            {
+                factorial *= i;
+                Thread.Sleep(100);
+            }
+
+            Console.WriteLine($"Вторичный поток закончил выполнять свою работу: { Thread.CurrentThread.ManagedThreadId }");
+
+            Console.WriteLine($"Двойной факториал: {factorial}");
+        }
+
+        public static void Fibonachi(object num)
+        {
+            Console.WriteLine($"Вторичный поток начал выполнять свою работу:  { Thread.CurrentThread.ManagedThreadId }");
+
+            int n = (int)num;
+
+            int j = 1;
+            for (int i = 1; i <= n; i += j)
+            {
+                Console.Write($"{i} ");
+                j = i - j;
+                Thread.Sleep(500);
+            }
+
+            Console.WriteLine($"Вторичный поток закончил выполнять свою работу: { Thread.CurrentThread.ManagedThreadId }");
+        }
+    }
+}
---
+++ WorkWithThreads2/WorkWithThreads_002/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+
+namespace WorkWithThreads_002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Main thread - { Thread.CurrentThread.ManagedThreadId } start work. ");
+
+            Thread workThread = new Thread(new ParameterizedThreadStart(new Action<object>(MyWork.DoWork)));
+
+            workThread.Start("*");
+
+
+            for (int i = 0; i < 20; i++)
+            {
+                Console.Write(".");
+                Thread.Sleep(30);
+            }
+
+            if (workThread.IsAlive)
+            {
+                workThread.Join();
+            }
+
+            Console.WriteLine($"Main thread - { Thread.CurrentThread.ManagedThreadId } end work. ");
+            Console.Read();
+        }
+    }
+
+    class MyWork
+    {
+        public static void DoWork(object ch)
+        {
+            Console.WriteLine($"Second thread - { Thread.CurrentThread.ManagedThreadId } start work. ");
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Write(ch);
+                Thread.Sleep(55);
+            }
+
+            Console.WriteLine($"\nSecond thread - { Thread.CurrentThread.ManagedThreadId } end work. ");
+            Console.WriteLine(Thread.CurrentThread.IsBackground); 
+        }
+    }
+}
